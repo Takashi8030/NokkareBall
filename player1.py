d0a3bebe5f9e44ffe68d1d8717303bfa34ac0957
Takashi8030 <takashi8030@gmail.com>
2024-03-31 16:29:28 +0900

    仮完成

--- player1.py
+++ player1.py
@@ -5,4 +5,4 @@ def action_ball_1(ball, space, stage_center, stage_radius):
     ball.mass = 9999999
     ball.shape.elasticity = 0
     ball.force = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * 999999999
-    ball.velocity = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * 99
+    ball.velocity = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * 9
