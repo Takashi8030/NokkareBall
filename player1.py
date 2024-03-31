f7c61de533c6cef0f95b6236fb4021d9c0347bd8
Takashi8030 <takashi8030@gmail.com>
2024-03-31 16:55:33 +0900

    各値に最大値、最小値を設定した。

--- player1.py
+++ player1.py
@@ -1,8 +1,8 @@
 import pymunk
-from statics import width, height
+from statics import width, height, mass_max, force_max, elasticity_min, velocity_max
 
 def action_ball_1(ball, space, stage_center, stage_radius):
-    ball.mass = 9999999
-    ball.shape.elasticity = 0
-    ball.force = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * 999999999
-    ball.velocity = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * 9
+    ball.mass = mass_max
+    ball.shape.elasticity = elasticity_min
+    ball.force = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * force_max
+    ball.velocity = (pymunk.Vec2d(width // 2, height // 2) - ball.position) * velocity_max
--- statics.py
+++ statics.py
@@ -1,1 +1,6 @@
 width, height = 1000, 1000
+mass_max = 1000
+force_max = 1000
+elasticity_min = 0.01
+elasticity_max = 1
+velocity_max = 10
